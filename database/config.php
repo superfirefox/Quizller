ae9a81321c6c411b762d7aa1f2af30e1c0a32025
superfirefox <flameoffirefox@gmail.com>
2023-03-27 21:51:12 +0200

    Update config.php

--- database/config.php
+++ database/config.php
@@ -1,8 +1,18 @@
 <?php
 //database configurations
-define("DB_HOST","localhost");
+define("DB_HOST","localhost"); 
+define("DB_UNAME","root");
+define("DB_PASS","");
+define("DB_DNAME","secondo");
+$conn=mysqli_connect(DB_HOST,DB_UNAME,DB_PASS,DB_DNAME);
+?>
+
+
+
+/*
+define("DB_HOST","localhost"); 
 define("DB_UNAME","YOUR_DATABASE_USER_NAME");
 define("DB_PASS","YOUR_DATABASE_PASSWORD");
 define("DB_DNAME","YOUR_DATABASE_NAME");
-$conn=mysqli_connect(DB_HOST,DB_UNAME,DB_PASS,DB_DNAME);
-?>
+
+*/
